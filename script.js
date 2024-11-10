effb94d2f860a74f5f94ce04d0b88df52c6f758b
Adinda Laras Sri Rahtami <adinlaras30@gmail.com>
2024-11-10 21:49:29 +0700

    js forgotpass

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('login-button').addEventListener('click', function() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const errorMessage = document.getElementById('error-message');
-
-});

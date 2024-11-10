8defdfcc81096f785bab6b7e032f27e97f9bd5d2
sagungputri <sagungputri@student.telkomuniversity.ac.id>
2024-11-10 12:00:01 +0700

    js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('login-button').addEventListener('click', function() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+
+});
